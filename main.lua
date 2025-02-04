df016b4daeaea4aafe19fcd1f67b4eb9be133676
Kartik Chaturvedi <kcfantastic@gmail.com>
2025-02-04 15:02:05 +0530

    Added Metatables

--- main.lua
+++ main.lua
@@ -366,21 +366,21 @@ print(lassy.breed)
 lassy:bark()
 ]]
 
-local function addTableValues(x, y)
-    return x.num + y.num
+--[[
+local function addTableValues(v1, v2)
+    return {x = v1.x + v2.x, y = v1.y + v2.y}
 end
 
 local metatable = {
-    __add = addTableValues,
-    __sub = function(x, y)
-        return x.num - y.num
-    end
+    __add = addTableValues 
 }
 
-local tbl1 = {num = 50}
-local tbl2 = {num = 10}
+local tbl1 = {x = 10, y = 20}
+local tbl2 = {x = 5, y = 9}
 
 setmetatable(tbl1, metatable)
 
-local ans = tbl1 - tbl2
-print(ans)
+local vec = tbl1 + tbl2
+
+print("x: " .. vec.x .. "\ny: " .. vec.y)
+]]
